58607f88a239f406fa5d6f98e03c4575dcf10582
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-12-02 20:52:17 -0500

    getProducts

--- front/src/containers/ProductListPage.jsx
+++ front/src/containers/ProductListPage.jsx
@@ -2,26 +2,27 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './product-list-page.css'
 import { Link, Outlet } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from '../redux/productsDucks'
 
 const ProductListPage = () => {
-  const [product, setProduct] = useState([])
+  const {products} = useSelector(state => state.products)
+  const dispatch = useDispatch()
+
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/products').then(res => setProduct(res.data))
-  }, [])
-  console.log(product)
+    dispatch(getProducts())
+  }, [dispatch])
   return (
     <>
       <main className='container-product-list'>
       <h2>Electronics</h2>
       <section className='product-list'>
         {
-          product.map(ele => {
-            let image = `http://localhost:5000${ele.image}`
-            console.log(image)
+          products?.map(ele => {
             return(
             <Link to={`detail/${ele._id}`} key={ele._id} className='product-list-card'>
-            <img src={image} alt={ele.name} />
+            <img src={ele.image} alt={ele.name} />
             <span>$ {ele.price}</span>
             <h3><small>{ele.name}</small></h3>
           </Link>
--- front/src/redux/productsDucks.js
+++ front/src/redux/productsDucks.js
@@ -1,5 +1,15 @@
+import axios from "axios";
 import { types } from "./types/types";
-
+// - El servidor se encuentra en:
+//   `http://localhost:5000/`
+// - La lista de productos se encuentra:
+//   `http://localhost:5000/api/products`
+// - Puedes encontrar cada producto por su ID:
+//   `http://localhost:5000/api/products/1`
+// - Las imágenes se encuentran en:|
+//   `http://localhost:5000/images/{{nombre-de-la-imagen}}`
+const urlProducts = 'http://localhost:5000/api/products';
+const urlImages = 'http://localhost:5000'
 // 
 const initialState = {
   products: [],
@@ -22,3 +32,24 @@ const productsDucks = (state = initialState, action) => {
 export default productsDucks
 // Ations
 
+
+const insertProduct = (products) => ({
+  type: types.GET_PRODUCTS,
+  payload: products
+})
+
+export const getProducts = () => async (dispatch) => {
+  try {
+    const res = await axios.get(urlProducts)
+    const data = await res.data
+    const newData = []
+    data.forEach(ele => {
+      newData.push({...ele, image: urlImages+ele.image})
+    });
+    dispatch(insertProduct(newData))
+
+  } catch (err) {
+    let message =  err.response?.statusText || "Ocurrió un error";
+    throw message
+  }
+}
